c447451afb793f0673765687b500124c9abd469b
kubosho <ta2@o2p.jp>
2014-09-24 14:27:41 +0900

    Add a help text

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+const helpText = `md2ghost - Convert a markdown files into Ghost posts.
+
+Usage: md2ghost [option] <file|directory>
+
+Options:
+
+  -o, --output  Specify an output directory for Ghost posts
+  -h, --help    Print this message and quit
+  -v, --version Print version information and quit
+
+Example:
+
+  $ md2ghost .
+  $ md2ghost -o path/to/output_directory path/to/your_directory
+`
