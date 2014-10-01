3aa935b84c97a17b25e2c3dc6f512681861e2f12
kubosho <ta2@o2p.jp>
2014-10-01 22:09:32 +0900

    Change a specified flags

--- md2ghost.go
+++ md2ghost.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
+
+	"github.com/jessevdk/go-flags"
 )
 
 const version = "v0.0.1"
 
-var (
-	flHelp    = flag.Bool("help", false, "Print this message and quit")
-	flVersion = flag.Bool("version", false, "Print version information and quit")
-)
+type opts struct {
+	Help    bool   `short:"h" long:"help" description:"Print this message and quit"`
+	Version bool   `short:"v" long:"version" description:"Print version information and quit"`
+	OutDir  string `short:"o" long:"output" description:"Specify an output directory for Ghost posts"`
+}
 
 func init() {
-	flag.BoolVar(flHelp, "h", false, "Print this message and quit")
-	flag.BoolVar(flVersion, "v", false, "Print version information and quit")
 }
 
 func main() {
-	flag.Parse()
-
-	if *flHelp || flag.NArg() == 0 {
+	opts := &opts{}
+	p := flags.NewParser(opts, flags.PrintErrors)
+	args, err := p.Parse()
+	if err != nil {
 		fmt.Fprintf(os.Stderr, helpText)
-		os.Exit(0)
+		return
 	}
 
-	if *flVersion {
+	if opts.Help {
+		fmt.Fprintf(os.Stderr, helpText)
+		return
+	}
+
+	if opts.Version {
 		fmt.Fprintf(os.Stderr, "md2ghost: %s\n", version)
-		os.Exit(0)
+		return
 	}
 }
 
