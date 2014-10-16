db181d96becd6b85314523fd06c86ed53258bf2a
kubosho <ta2@o2p.jp>
2014-10-17 02:09:49 +0900

    Add Ghost JSON format structure

--- md2ghost.go
+++ md2ghost.go
@@ -17,6 +17,75 @@ type opts struct {
 	OutDir  string `short:"o" long:"output" description:"Specify an output directory for Ghost posts"`
 }
 
+type GhostJSON struct {
+	Meta Meta `json:"meta"`
+	Data Data `json:"data"`
+}
+
+type Meta struct {
+	ExportedOn int64  `json:"exported_on"`
+	Version    string `json:"version"`
+}
+
+type Data struct {
+	Posts []struct {
+		AuthorID        int64       `json:"author_id"`
+		CreatedAt       int64       `json:"created_at"`
+		CreatedBy       int64       `json:"created_by"`
+		Featured        int64       `json:"featured"`
+		HTML            string      `json:"html"`
+		ID              int64       `json:"id"`
+		Image           interface{} `json:"image"`
+		Language        string      `json:"language"`
+		Markdown        string      `json:"markdown"`
+		MetaDescription interface{} `json:"meta_description"`
+		MetaTitle       interface{} `json:"meta_title"`
+		Page            int64       `json:"page"`
+		PublishedAt     int64       `json:"published_at"`
+		PublishedBy     int64       `json:"published_by"`
+		Slug            string      `json:"slug"`
+		Status          string      `json:"status"`
+		Title           string      `json:"title"`
+		UpdatedAt       int64       `json:"updated_at"`
+		UpdatedBy       int64       `json:"updated_by"`
+	} `json:"posts"`
+	PostsTags []struct {
+		PostID int64 `json:"post_id"`
+		TagID  int64 `json:"tag_id"`
+	} `json:"posts_tags"`
+	RolesUsers []struct {
+		RoleID int64 `json:"role_id"`
+		UserID int64 `json:"user_id"`
+	} `json:"roles_users"`
+	Tags []struct {
+		Description string `json:"description"`
+		ID          int64  `json:"id"`
+		Name        string `json:"name"`
+		Slug        string `json:"slug"`
+	} `json:"tags"`
+	Users []struct {
+		Accessibility   interface{} `json:"accessibility"`
+		Bio             interface{} `json:"bio"`
+		Cover           interface{} `json:"cover"`
+		CreatedAt       int64       `json:"created_at"`
+		CreatedBy       int64       `json:"created_by"`
+		Email           string      `json:"email"`
+		ID              int64       `json:"id"`
+		Image           interface{} `json:"image"`
+		Language        string      `json:"language"`
+		LastLogin       interface{} `json:"last_login"`
+		Location        interface{} `json:"location"`
+		MetaDescription interface{} `json:"meta_description"`
+		MetaTitle       interface{} `json:"meta_title"`
+		Name            string      `json:"name"`
+		Slug            string      `json:"slug"`
+		Status          string      `json:"status"`
+		UpdatedAt       int64       `json:"updated_at"`
+		UpdatedBy       int64       `json:"updated_by"`
+		Website         interface{} `json:"website"`
+	} `json:"users"`
+}
+
 func main() {
 	opts := &opts{}
 	p := flags.NewParser(opts, flags.PrintErrors)
